bbc916591242026d032445ba726333e984131aaf
JAMBITI <biti.jamal@gmail.com>
2023-11-26 05:28:46 -0800

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,9 @@
+# Set up server configuration with puppet
+file_line{'Set alias name':
+  path => '/etc/ssh/ssh_config',
+  line => 'Host 54.144.87.36
+    HostName 54.210.89.176
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no',
+}
